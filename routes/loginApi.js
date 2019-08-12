ce5352d12a83c580c07dc6e1329a158a9307e121
Alina <alina.diezgr@gmail.com>
2019-08-12 14:55:39 -0500

    Edited loginApi

--- routes/loginApi.js
+++ routes/loginApi.js
@@ -8,7 +8,6 @@ const router = express.Router();
 
 router.post("", (req, res, next) => {
     let fetchedUsr;
-    console.log(req.body);
     Usuario.findOne({ email: req.body.email })
         .then(usuario => {
             if (!usuario) {
@@ -17,7 +16,6 @@ router.post("", (req, res, next) => {
                 });
             }
             fetchedUsr = usuario;
-            console.log(fetchedUsr);
             return bcrypt.compare(req.body.password, usuario.password);
         })
         .then(result => {
